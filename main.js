9f5a15819d2f6503f33e5b27b0d30be239e5e72a
rainyjune <rainyjune@live.cn>
2015-06-09 13:38:09 +0800

    Make sure Canvas is supported on the browser

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("I'm ready");
+// Init the app after page loaded.
+window.onload = initPage;
+
+// The entry function.
+function initPage() {
+  if (!canvasSupport()) { // If Canvas is not supported on the browser, do nothing but tell the user.
+    alert("Sorry, your browser does not support HTML5 Canvas");
+    return false;
+  }
+  drawScreen();
+}
+
+function drawScreen() {
+  var canvas = document.getElementById("theCanvas");
+  if (!canvas) {
+    throw new Error("The canvas element does not exists!");
+  }
+}
+
+// Does the browser support HTML5 Canvas ?
+function canvasSupport() {
+  var canvas = document.createElement("canvas");
+  return !!(canvas.getContext && canvas.getContext("2d"));
+} 
