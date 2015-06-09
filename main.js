3d2857c2bc2c1f0e03f3183277f1516d7f513d0a
rainyjune <rainyjune@live.cn>
2015-06-09 13:51:09 +0800

    Add the log() function

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Init the app after page loaded.
 window.onload = initPage;
 
+var debug = true;
+
 // The entry function.
 function initPage() {
   if (!canvasSupport()) { // If Canvas is not supported on the browser, do nothing but tell the user.
@@ -15,6 +17,7 @@ function drawScreen() {
   if (!canvas) {
     throw new Error("The canvas element does not exists!");
   }
+  var ctx = canvas.getContext("2d");
 }
 
 // Does the browser support HTML5 Canvas ?
@@ -22,3 +25,13 @@ function canvasSupport() {
   var canvas = document.createElement("canvas");
   return !!(canvas.getContext && canvas.getContext("2d"));
 } 
+
+function log() {
+  if (!debug) return ;
+  var args = Array.prototype.slice.call(arguments);
+  if (window.console && window.console.log) {
+    console.log.apply(console, args);
+  } else {
+    alert(args.join("\n"));
+  }
+}
