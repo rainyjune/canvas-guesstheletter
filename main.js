0eaadf0e7e9bc25a3ac44343427c04a587e01cad
rainyjune <rainyjune@live.cn>
2015-06-10 09:55:14 +0800

    Remove rand()

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ function canvasApp() {
   }
 
   function getRandomChar() {
-    var random = rand(0, allowedChars.length - 1);
+    var random = Math.floor(Math.random() * allowedChars.length);
     correctCharIndex = random;
     return allowedChars[random];
   }
@@ -214,15 +214,4 @@ function range(low, high, step) {
   }
 
   return matrix;
-}
-//http://phpjs.org/functions/rand/
-function rand(min, max) {
-  var argc = arguments.length;
-  if (argc === 0) {
-    min = 0;
-    max = 2147483647;
-  } else if (argc === 1) {
-    throw new Error('Warning: rand() expects exactly 2 parameters, 1 given');
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
